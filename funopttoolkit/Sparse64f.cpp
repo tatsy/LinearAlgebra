d14b2090bd4d941bdb490928552e7b9adf848906
tatsy <tatsy.mail@gmail.com>
2013-11-16 09:55:57 +0900

    [improve] sparse matrix added.

--- funopttoolkit/Sparse64f.cpp
+++ funopttoolkit/Sparse64f.cpp
@@ -1,7 +1,8 @@
+#include <map>
 #include <algorithm>
 using namespace std;
 
-#include "Triplet.h"
+#include "funopt_macros.h"
 
 #define __EXPORT__
 #include "Sparse64f.h"
@@ -26,7 +27,7 @@ Sparse64f::Sparse64f(vector<Triplet> triplets, int rows, int cols) :
 {
 	data = new double[nnz];
 	row_idx = new int[nnz];
-	col_heads = new int[nrows];
+	col_heads = new int[ncols+1];
 
 	sort(triplets.begin(), triplets.end());
 
@@ -35,9 +36,10 @@ Sparse64f::Sparse64f(vector<Triplet> triplets, int rows, int cols) :
 		row_idx[i] = triplets[i].i;
 		if(i == 0) col_heads[0] = 0;
 		else if(triplets[i].j != triplets[i-1].j) {
-			col_heads[triplets[i-1].j] = i;
+			col_heads[triplets[i].j] = i;
 		}
 	}
+	col_heads[ncols] = nnz;
 }
 
 Sparse64f::Sparse64f(const Sparse64f& sp) :
@@ -50,11 +52,11 @@ Sparse64f::Sparse64f(const Sparse64f& sp) :
 {
 	data = new double[nnz];
 	row_idx = new int[nnz];
-	col_heads = new int[nrows];
+	col_heads = new int[ncols+1];
 
 	memcpy(data, sp.data, sizeof(double) * nnz);
 	memcpy(row_idx, sp.row_idx, sizeof(int) * nnz);
-	memcpy(col_heads, sp.col_heads, sizeof(int) * nrows);
+	memcpy(col_heads, sp.col_heads, sizeof(int) * (ncols+1));
 }
 
 Sparse64f::~Sparse64f()
@@ -76,13 +78,54 @@ Sparse64f& Sparse64f::operator=(const Sparse64f& sp)
 
 	data = new double[nnz];
 	row_idx = new int[nnz];
-	col_heads = new int[nrows];
+	col_heads = new int[ncols+1];
 
 	memcpy(data, sp.data, sizeof(double) * nnz);
 	memcpy(row_idx, sp.row_idx, sizeof(int) * nnz);
-	memcpy(col_heads, sp.col_heads, sizeof(int) * nrows);
+	memcpy(col_heads, sp.col_heads, sizeof(int) * (ncols+1));
 
 	return *this;
+}
+
+Sparse64f Sparse64f::operator*(const Sparse64f& sp) const
+{
+	massert(ncols == sp.nrows, "Matrix size is invalid");
+	int rows = nrows;
+	int cols = sp.ncols;
+	int len  = ncols;
+
+	map<int,double>::iterator it;
+	map<int,double> col_elems;
+	vector<Triplet> triplets;
+	for(int k=0; k<len; k++) {
+		col_elems.clear();
+		int s1 = sp.col_heads[k];
+		int t1 = sp.col_heads[k+1];
+		for(int i=s1; i<t1; i++) {
+			int r1  = row_idx[i];
+			int s2 = col_heads[r1];
+			int t2 = col_heads[r1+1];
+			for(int j=s2; j<t2; j++) {
+				int r2 = row_idx[j];
+				col_elems[r2] += data[j] * sp.data[i];
+			}
+		}
+
+		for(it=col_elems.begin(); it!=col_elems.end(); ++it) {
+			triplets.push_back(Triplet(it->first, k, it->second));
+		}
+	}
+	return Sparse64f(triplets, rows, cols);
+}
+
+double Sparse64f::operator()(int i, int j) const
+{
+	for(int k=col_heads[j]; k<col_heads[j+1]; k++) {
+		if(row_idx[k] == i) {
+			return data[k];
+		}
+	}
+	return 0.0;
 }
 
 Sparse64f Sparse64f::eye(int n)
